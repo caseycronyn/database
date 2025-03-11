7fd390627a5e3e5395dd976b4be37047aef492e7
Casey Cronyn <casey.cronyn@gmail.com>
2025-03-11 22:28:30 +0000

    new

--- cw-db/src/main/java/edu/uob/example-transcript.java
+++ cw-db/src/main/java/edu/uob/example-transcript.java
@@ -1,110 +1,114 @@
+import java.util.HashMap;
+
+class ExampleTranscript {
 String[] longResponse = {
-        "
+        """
             [OK]
             id name mark pass
             1 Simon 65 TRUE
             2 Sion 55 TRUE
             3 Rob 35 FALSE
             4 Chris 20 FALSE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass
             1 Simon 65 TRUE
             3 Rob 35 FALSE
             4 Chris 20 FALSE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass
             1 Simon 65 TRUE
             2 Sion 55 TRUE
-        ",
-        "
+        """,
+        """
             [OK]
             id task submission
             1 OXO 3
             2 DB 1
             3 OXO 4
             4 STAG 2
-        ",
-        "
+        """,
+        """
             [OK]
             id coursework.task marks.name marks.mark marks.pass
             1 OXO Rob 35 FALSE
             2 DB Simon 65 TRUE
             3 OXO Chris 20 FALSE
             4 STAG Sion 55 TRUE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass
             4 Chris 38 FALSE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass
             1 Simon 65 TRUE
             3 Rob 35 FALSE
             4 Chris 38 FALSE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass
             4 Chris 38 FALSE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass
             1 Simon 65 TRUE
             4 Chris 38 FALSE
-        ",
-        "
+        """,
+        """
             [OK]
             id
             3
             4
-        ",
-        "
+        """,
+        """
             [OK]
             name
             Simon
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass
             1 Simon 65 TRUE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass age
             1 Simon 65 TRUE
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark pass age
             1 Simon 65 TRUE 35
-        ",
-        "
+        """,
+        """
             [OK]
             id name mark age
             1 Simon 65 35
-        ",
-        "
+        """,
+        """
             [ERROR]: Semi colon missing at end of line
-        ",
-        "
+        """,
+        """
             [ERROR]: Table does not exist
-        ",
-        "
+        """,
+        """
             [ERROR]: Attribute does not exist 
-        ",
+        """,
 }
 
 String good = "[OK]";
 
-// query, response. True = [OK], False = [Error] 
-HashMap<String, String> queries = {
+// query, response. True = [OK], False = [Error]
+List<HashMap<String, String>> queries = new ArrayList<>();
+
     "CREATE DATABASE markbook;", good,
     "USE markbook;", good,
     "CREATE TABLE marks (name, mark, pass);", good,
@@ -116,13 +120,13 @@ HashMap<String, String> queries = {
     "SELECT * FROM marks WHERE name != 'Sion';", longResponse[1],
     "SELECT * FROM marks WHERE pass == TRUE;", longResponse[2],
     // Assuming there is a table called “coursework” in the database (and that table has been filled with data)
-    "SELECT * FROM coursework;", longResponse[3], 
+    "SELECT * FROM coursework;", longResponse[3],
     // For JOINs: discard the ids from the original tables discard the columns that the tables were matched on create a new unique id for each of row of the table produced attribute names are prepended with name of table from which they originated
     "JOIN coursework AND marks ON submission AND id;", longResponse[4],
     "UPDATE marks SET mark = 38 WHERE name == 'Chris';", good,
     "SELECT * FROM marks WHERE name == 'Chris';", longResponse[5],
     "SELECT * FROM marks;", longResponse[6],
-    "DELETE FROM marks WHERE name == 'Sion';", good, 
+    "DELETE FROM marks WHERE name == 'Sion';", good,
     "SELECT * FROM marks WHERE (pass == FALSE) AND (mark > 35);", longResponse[7],
     "SELECT * FROM marks WHERE name LIKE 'i';", longResponse[8],
     "SELECT id FROM marks WHERE pass == FALSE;", longResponse[9],
@@ -142,4 +146,13 @@ HashMap<String, String> queries = {
     "SELECT height FROM marks WHERE name == 'Chris';", longResponse[17],
     "DROP TABLE marks;", good,
     "DROP DATABASE markbook;", good,
+}
+
+void addEntry(String query, String response) {
+        HashMap<String, String> entry = new HashMap<>();
+        entry.put("query", query);
+        entry.put("response", response);
+        list.add(entry);
+}
+
 }
