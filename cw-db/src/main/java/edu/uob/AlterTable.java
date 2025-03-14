ac06bb53a6ee85c1bf725d42a31bdf4853ab83b0
Casey Cronyn <casey.cronyn@gmail.com>
2025-03-14 14:58:27 +0000

    update

--- cw-db/src/main/java/edu/uob/AlterTable.java
+++ cw-db/src/main/java/edu/uob/AlterTable.java
@@ -3,15 +3,21 @@ package edu.uob;
 public class AlterTable extends Parser {
     @Override
     public DBCommand parse(TokenBank tokenBank) {
-        tokenBank.nextToken();
-        if (tokenBank.nextToken().equals("ADD")) {
-            DBCommand addAttribute = new AddAttribute();
-            return addAttribute.parse(tokenBank);
+        return this;
+    }
+
+    @Override
+    public void executeCommand(DBServer server, TokenBank tokenBank) {
+        String alterationType = tokenBank.getTokenFromType("alterationType").getName();
+        String tableName = tokenBank.getTokenFromType("tableName").getName();
+        String attributeName = tokenBank.getTokenFromType("attributeName").getName();
+        Table table = server.databases.get(server.getCurrentDatabase()).tables.get(tableName);
+
+        if (alterationType.equals("ADD")) {
+            table.addNewAttribute(attributeName);
         }
-        else if (tokenBank.nextToken().equals("DROP")) {
-            DBCommand dropAttribute = new DropAttribute();
-            return dropAttribute.parse(tokenBank);
+        else if (alterationType.equals("DROP")) {
+            table.removeAttribute(attributeName);;
         }
-        return null;
     }
 }
